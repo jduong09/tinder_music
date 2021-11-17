0de501bca3fe3fc3d54c4975e6684a69f705a276
Justin Duong <jduong@umich.edu>
2021-11-16 19:53:58 -0500

    Created random buttons for css animation

--- src/components/genreSelector.jsx
+++ src/components/genreSelector.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
-// Get available genre seeds from API for GenreSelector
+// Get available genre seeds from API for GenreSelector (done)
+// There are around 126 genres.
+// What to do with this info.
+// Create 
 function GenreSelector() {
 
-  const [genres, setGenres] = useState('');
+  const [genres, setGenres] = useState([]);
   
   useEffect(() => {
     async function getGenres() {
@@ -13,16 +16,25 @@ function GenreSelector() {
     }
 
     getGenres();
-  }, [])
+  }, []);
+  let randomNums = [];
 
-  const genreList = genres.map((genre, idx) => {
-    return <li key={idx}>{genre}</li>
+  for (let i = 0; i < 5; i++) {
+    const min = Math.floor(0);
+    const max = Math.ceil(genres.length);
+    const number = Math.floor(Math.random() * (max - min) + min);
+    randomNums.push(number);
+    console.log(randomNums)
+  };
+
+  const genreList = randomNums.map((num, i) => {
+    return <li className="genre" key={i}><button>{genres[num]}</button></li>
   });
-  
+
   if (genres) {
-    return <ul>{genreList}</ul>
+    return <ul className="genres">{genreList}</ul>;
   } else {
-    return <div>Loading</div>
+    return <div>Loading</div>;
   }
 };
 
