db459cdc322ca61bdd7ae70afb1a7c00136c539b
Justin Duong <jduong@umich.edu>
2021-11-18 18:44:30 -0500

    changed two animations into one animation, used onAnimationIteration to change DOM element to create infinitely changing genre names

--- src/components/genreSelector.jsx
+++ src/components/genreSelector.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 // Get available genre seeds from API for GenreSelector (done)
 // There are around 126 genres.
@@ -9,21 +9,28 @@ class GenreSelector extends React.Component {
     super(props);
 
     this.state = {
-      genres: []
+      genres: null,
+      genreList: []
     };
 
     this.changeButtonClass = this.changeButtonClass.bind(this);
+    this.updateGenreList = this.updateGenreList.bind(this);
   }
   
   async componentDidMount() {
     const response = await fetch('/auth/genres');
     const json = await response.json();
-    this.setState({ genres: json.genres });
+
+    const genreList = [];
+
+    this.getRandomNums(json.genres).forEach(num => {
+      genreList.push(json.genres[num]);
+    });
+
+    this.setState({ genres: json.genres, genreList: genreList });
   };
 
-  getRandomNums() {
-    const { genres } = this.state;
-    console.log(genres);
+  getRandomNums(genres = this.state.genres) {
     let randomNums = [];
 
     for (let i = 0; i < 5; i++) {
@@ -37,17 +44,36 @@ class GenreSelector extends React.Component {
   };
 
   changeButtonClass(e) {
-    console.log(e.currentTarget);
     const target = e.currentTarget;
     target.classList.remove("yo");
     target.classList.add("hello");
+  };
+
+  // needs to be called once, update the genre list after the fadeIn, fadeOut is complete.
+  updateGenreList() {
+    const randomNums = this.getRandomNums();
+
+    const newGenreList = [];
+
+    randomNums.forEach(num => {
+      newGenreList.push(this.state.genres[num]);
+    });
+
+    this.setState({ genreList: newGenreList });
+    // add 5 new random genre buttons to the dom.
+    // get random numbers
+    // iterate over the random numbers
+    // remove button at that index in the genre list. 
+    // create a new button, with the random num being the genre in the genres array.
+    // add to the genre list.
+
+    // give them the same properties so they can fadeIn, fadeOut. 
   }
 
   render() {
-    const genreList = this.getRandomNums().map((num, i) => {
-      return <li className="genre" key={i}><button className="yo" onAnimationEnd={this.changeButtonClass}>{this.state.genres[num]}</button></li>
-    });
-
+    const genreList = this.state.genreList.map((genre, i) => {
+      return <li className="genre" key={i}><button className="yo" onAnimationIteration={this.updateGenreList}>{genre}</button></li>
+    })
     if (this.state.genres) {
       return <ul className="genres">{genreList}</ul>;
     } else {
