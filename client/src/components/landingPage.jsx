1e68f6c260ee80de3b5eeec55ddbc30d5808fc17
Justin Duong <jduong@umich.edu>
2022-04-05 21:30:28 -0400

    Adding logo and heroImage assets. Began redesign of landingPage.

--- client/src/components/landingPage.jsx
+++ client/src/components/landingPage.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Nav from './nav';
-import headphonePicture from '../kaboompics-com-6251.jpg';
+import heroImage from '../assets/heroImage.png';
+import '../css/landingPage.css';
 
 const LandingPage = () => (
   <section className="landing-page">
     <header className="app-header">
       <Nav />
     </header>
-    <section>
-      <section className="landing-page-about">
-        <h2>Objective</h2>
-        <ul>
-          <li>Pick a genre you want to hear music from</li>
-          <li>Choose the left song or the right song!</li>
-          <li>Save YOUR songs to a playlist to listen later!</li>
-        </ul>
-        <a className="login-button" href="/auth/login">Get Started!</a>
-      </section>
-      <img id="headphone-stock" src={headphonePicture} alt="headphone-stock"/>
-    </section>
+    <main>
+      <div className="landingPageHeader" >
+        <h2>Fall In Love With Music.</h2>
+        <img id="heroImage" src={heroImage} alt="cat listening to music" />
+      </div>
+      <ul className="landingPageAbout" >
+        <li><a className="login-button" href="/auth/login">Get Started!</a></li>
+        <li>Select A Genre</li>
+        <li>Choose Between Songs</li>
+        <li>Keep It Forever</li>
+      </ul>
+    </main>
   </section>
 )
 
